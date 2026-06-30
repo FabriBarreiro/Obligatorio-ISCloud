87d4bdf3ebf418c4d5ef077e01d5a6a204e716d1
Santiago  Nahuel Hoaguy  Alvarez <sh338979@fi365.ort.edu.uy>
2026-06-29 23:44:20 -0300

    Correccion de nombres en outputs

--- IaC-Terraform/modules/ecr/outputs.tf
+++ IaC-Terraform/modules/ecr/outputs.tf
@@ -1,20 +1,28 @@
 
 
 output "repository_names" {
-  description = "Repositorios creados"
+  description = "Nombres de los repositorios creados"
 
   value = {
-    for repo in aws_ecr_repository.repositories :
-    repo.name => repo.repository_url
+    for name, repo in aws_ecr_repository.repositories :
+    name => repo.name
   }
 }
 
-output "repository_url" {
-  description = "URL del repositorio ECR creado."
-  value       = aws_ecr_repository.app_repository.repository_url
+output "repository_urls" {
+  description = "URLs de los repositorios creados"
+
+  value = {
+    for name, repo in aws_ecr_repository.repositories :
+    name => repo.repository_url
+  }
 }
 
-output "repository_arn" {
-  description = "ARN del repositorio ECR creado."
-  value       = aws_ecr_repository.app_repository.arn
+output "repository_arns" {
+  description = "ARNs de los repositorios creados"
+
+  value = {
+    for name, repo in aws_ecr_repository.repositories :
+    name => repo.arn
+  }
 }
