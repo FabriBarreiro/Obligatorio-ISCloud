0e9c6f827b4a8265be8160d908a9ab8c5fb720e4
Santiago  Nahuel Hoaguy  Alvarez <sh338979@fi365.ort.edu.uy>
2026-06-30 14:28:41 -0300

    Correcciones de despliegue para EKS, permito que ElastiCache acepte conexiones desde los worker nodes de EKS

--- IaC-Terraform/modules/security-groups/main.tf
+++ IaC-Terraform/modules/security-groups/main.tf
@@ -137,6 +137,22 @@ resource "aws_security_group" "elasticache_sg" {
     security_groups = [aws_security_group.eks_nodes_sg.id]
   }
 
+   ingress {
+    description = "Permite acceso Redis desde la VPC"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    description = "Permite trafico saliente"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.project_name}-${var.environment}-elasticache-sg"
   }
